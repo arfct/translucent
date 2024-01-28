cf872c3e64e7286fd9af3bd34241fc72b5e51ac0
Nicholas Jitkoff <nicholas@jitkoff.com>
2024-01-28 06:47:09 -0600

    Initial Commit

---
+++ ittybittywidgets/ittybittywidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ittybittywidgetsApp.swift
+//  ittybittywidgets
+//
+//  Created by Nicholas Jitkoff on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ittybittywidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
