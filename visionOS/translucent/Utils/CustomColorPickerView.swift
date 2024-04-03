502dacb2b6fa21fa49ead12483c079a09acb6a0f
Nicholas Jitkoff <nicholas@jitkoff.com>
2024-04-03 07:38:35 -0700

    Update CustomColorPickerView.swift

--- visionOS/translucent/Utils/CustomColorPickerView.swift
+++ visionOS/translucent/Utils/CustomColorPickerView.swift
@@ -7,19 +7,34 @@ struct CustomColorPickerView: View {
     var body: some View {
         
         colorValue
-            .frame(width: 48, height: 32, alignment: .center)
-            
-            
-  
-                        
-            .shadow(radius: 5.0)
-            .glassBackgroundEffect(in:RoundedRectangle(cornerRadius: 8).inset(by: 4))
-            .background(RoundedRectangle(cornerRadius: 8.0).fill(.regularMaterial).stroke(.tertiary, style: StrokeStyle(lineWidth: 1)))
+            .frame(width: 56, height: 32, alignment: .center)
+            .glassBackgroundEffect(in:RoundedRectangle(cornerRadius: 80).inset(by: 5))
+            .background {
+              RoundedRectangle(cornerRadius: 16).inset(by: 0)
+              
+                .fill(.shadow(.inner(color:colorValue.opacity(1.0), radius: 0.0, y: 1)))
+                .fill(.shadow(.drop(color:.white.opacity(0.9), radius: 0.0, y: 1)))
+                .fill(.shadow(.drop(color:.black.opacity(0.9), radius: 0.0, y: -1)))
+                .foregroundStyle(.linearGradient(
+                  colors: [.black.opacity(0.15),
+                           .black.opacity(0.12),
+                           .black.opacity(0.05)],
+                  startPoint: .top, endPoint: .bottom))
+
+            }
+      
             .overlay(ColorPicker("", selection:  $colorValue).labelsHidden().opacity(0.015))
- 
+      
     }
 }
 
 #Preview {
-  CustomColorPickerView(colorValue: .constant(.pink))
+  VStack{
+    CustomColorPickerView(colorValue: .constant(.pink))
+    Toggle(isOn: .constant(false)) {
+       
+    }.labelsHidden()
+
+  }
+  .padding(55).background(.linearGradient(colors: [.white, .black], startPoint: .center, endPoint: .center)).glassBackgroundEffect()
 }
